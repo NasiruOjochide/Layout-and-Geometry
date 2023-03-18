03db1d236128174cc761f88d78fdd294153ad983
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-03-18 20:18:32 +0100

    Initial Commit

---
+++ Layout and Geometry/Layout_and_GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_and_GeometryApp.swift
+//  Layout and Geometry
+//
+//  Created by Danjuma Nasiru on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Layout_and_GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
